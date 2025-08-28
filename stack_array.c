ea42574f2d482082a5dfa0c24a700add3e9bb11c
Shruthikeerthi11 <nethishruthi1106@gmail.com>
2025-08-28 12:34:06 +0530

    Add files via upload

---
+++ stack_array.c
@@ -0,0 +1,86 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#define size 2
+int st[size];
+
+int top=-1;
+
+void push(int item)
+{
+	top++;
+	st[top] = item;
+}
+   int peek()
+   {
+   	return st[top];
+   }
+int pop()
+{
+	int item;
+	item = st[top];
+	top--;
+	return(item);
+}
+
+void display()
+{
+	int i;
+	for(i=top;i>=0;i--)
+	  printf("\n%d",st[i]);
+}
+
+int main()
+{
+	int item,choice;
+	char ans;
+	printf("\n\tImplementation of stack");
+	do{
+	    printf("\nMain Menu");
+		printf("\n1.push \n2.pop \n3.peek \n4.display \n5.exit");
+		printf("\nEnter Your choice");
+		scanf("%d",&choice);
+		switch(choice){
+		
+		case 1:
+			if(top<size-1)
+			{
+				printf("\Enter the item to be pushed");
+				scanf("%d",&item);
+				push(item);
+			}
+			else
+			printf("overflow");
+			break;
+			case 2:
+				if(top==-1)
+				printf("underflow");
+				else
+				{
+					item=peek();
+					printf("The top most element is %d",item);
+					
+				}
+				break;
+				case 3:
+					if(top==-1)
+					printf("underflow");
+					else
+					{
+						item=peek();
+						printf("The top most element is %d",item);
+					}
+					break;
+				    case 4:
+					    display();
+					    break;
+					    case 5:
+						    exit(0);
+	}			
+	printf("\nDo you want to continue?");
+	ans=getche();
+}
+while(ans=='Y'||ans =='Y');
+return 0;
+	
+}
